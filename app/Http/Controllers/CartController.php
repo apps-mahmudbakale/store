bf1a65e93c5e28be6fd572455d6806e4c7f869a7
Mahmud Bakale <5157015-apps-mahmudbakale@users.noreply.gitlab.com>
2021-06-22 06:40:21 +0100

    Initial Commit

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+
+    public function shop()
+    {
+        $products = Product::all();
+        // dd($products);
+        return view('shop')->withTitle('E-COMMERCE STORE | SHOP')->with(['products' => $products]);
+    }
+
+    public function cart()  {
+        $cartCollection = \Cart::getContent();
+        dd($cartCollection);
+        return view('cart')->withTitle('E-COMMERCE STORE | CART')->with(['cartCollection' => $cartCollection]);;
+    }
+   public function add(Request$request){
+           \Cart::add(array(
+               'id' => $request->id,
+               'name' => $request->name,
+               'price' => $request->price,
+               'quantity' => $request->quantity,
+               'attributes' => array(
+                   'image' => $request->img,
+                   'slug' => $request->slug
+               )
+           ));
+           return redirect()->route('cart.index')->with('success_msg', 'Item is Added to Cart!');
+       }
+}
